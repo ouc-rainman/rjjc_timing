034e0d7936edc393cabdfca04a7f852c5383b46e
ouc-rainman <rainman.ouc@gmail.com>
2018-06-22 16:44:38 +0800

    version_4
    每个用户都有唯一的UserID

--- wafer2-quickstart-nodejs-master/server/controllers/UserRegistion.js
+++ wafer2-quickstart-nodejs-master/server/controllers/UserRegistion.js
@@ -5,8 +5,20 @@ module.exports = async ctx => {
   timestamp = timestamp / 1000;
   let userInfo = ctx.request.body.userInfo ? ctx.request.body.userInfo : 9998
   let result = await mysql.column('OpenId').where({ OpenId: userInfo.openId }).select().from('UserInfo')
-  if(result.length==0){
-    let result = await mysql('UserInfo').insert({OpenId:userInfo.openId, EnrollTime:timestamp, AvatarUrl:userInfo.avatarUrl, NickName:userInfo.nickName})
+  var text=""
+  while (true && result.length==0) {
+    text=""
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+    for (let i = 0; i < 8; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    let result2 = await mysql('UserInfo').select('UserID').where({ UserID: text })
+    if (result2.length == 0) break
   }
+  if(result.length==0){
+    let result = await mysql('UserInfo').insert({OpenId:userInfo.openId, EnrollTime:timestamp, AvatarUrl:userInfo.avatarUrl, NickName:userInfo.nickName, UserID:text})
+  }
+
   ctx.state.data = true
 }
