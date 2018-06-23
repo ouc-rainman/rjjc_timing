24904a635cd1facdf97f2381681217875f95758d
ouc-rainman <rainman.ouc@gmail.com>
2018-06-23 15:59:03 +0800

    更改后台用户学习历史错误

--- wafer2-quickstart-nodejs-master/server/controllers/GetHistory.js
+++ wafer2-quickstart-nodejs-master/server/controllers/GetHistory.js
@@ -3,7 +3,7 @@ const { mysql } = require('../qcloud')
 module.exports = async ctx => {
   let userInfo = ctx.request.body.userInfo ? ctx.request.body.userInfo : -1
   
-  let result = await mysql('Calender').where({ OpenId: userInfo.openId }).andWhereNotNull('EndTime').select()
+  let result = await mysql('Calender').where({ OpenId: userInfo.openId }).whereNotNull('EndTime').select()
   let result2 = await mysql('BedTime').where({ OpenId: userInfo.openId }).select()
   ctx.state.data = { 'Study': result, 'BedTime': result2 }
 }
